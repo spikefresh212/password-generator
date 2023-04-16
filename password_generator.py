b44c2278567b43ff43acfa3e9f3d15a46018cdef
spikefresh212 <47723469+spikefresh212@users.noreply.github.com>
2023-04-16 23:10:45 +0200

    Add files via upload

--- password_generator.py
+++ password_generator.py
@@ -11,7 +11,7 @@ window.configure(bg='white')
 def validate_lenght_pw():
   global generated_password
   generated_password = ""
-  if(val_check_button1 or val_check_button2 or val_check_button3 or val_check_button4):
+  if(val_check_button1.get() or val_check_button2.get() or val_check_button3.get() or val_check_button4.get()):
     try:
       tmp = int(pw_lenght_input.get())
       if(tmp > 100):
@@ -110,6 +110,8 @@ def generate_password(lenght_of_password):
       while(x == 1 or x == 3):
         x = random.randint(0,3)
       y = 0
+      if(x == 0):
+        y = random.randint(0, 24)
       if(x == 2):
         y = random.randint(0,25)
       tmp_pw.append(get_random_char(x,y))
@@ -161,6 +163,24 @@ def generate_password(lenght_of_password):
       elif(x == 3):
         y = random.randint(0,25)
       tmp_pw.append(get_random_char(x,y))
+    if(val_check_button1.get() == 0 and val_check_button2.get() == 0 and val_check_button3.get() == 0 and val_check_button4.get()):
+      x = 3
+      y = 0
+      if(x == 3):
+        y = random.randint(0,25)
+      tmp_pw.append(get_random_char(x,y))
+    if(val_check_button1.get() == 0 and val_check_button2.get() and val_check_button3.get() == 0 and val_check_button4.get() == 0):
+      x = 1
+      y = 0
+      if(x == 1):
+        y = random.randint(0,9)
+      tmp_pw.append(get_random_char(x,y))
+    if(val_check_button1.get() and val_check_button2.get() == 0 and val_check_button3.get() == 0 and val_check_button4.get() == 0):
+      x = 0
+      y = 0
+      if(x == 0):
+        y = random.randint(0,24)
+      tmp_pw.append(get_random_char(x,y))
   return tmp_pw
 
 def get_random_char(x,y):
@@ -193,7 +213,6 @@ numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 lowercase_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 uppercase_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 list_of_all_characters = [symbols, numbers, lowercase_letters, uppercase_letters]
-print(str(len(symbols)), str(len(numbers)), str(len(lowercase_letters)), str(len(uppercase_letters)))
 
 
 check_button1.pack(side= TOP)
